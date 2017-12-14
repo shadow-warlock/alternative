aa527a1bbc3692c4975d931d84405728895db60c
shadow_warlock <nik_mak@bk.ru>
2017-12-14 18:05:50 +0300

    twince push

--- src/Automaton.java
+++ src/Automaton.java
@@ -14,178 +14,199 @@ public class Automaton {
     }
 
     private void startState() throws Exception {
-        if(reader.isInputState()) {
+        while(reader.isInputState()) {
+            if(reader.get() == ')')
+                return;
             if (reader.get() >= 'a' && reader.get() <= 'z') {
-                letter();
+                String name = "" + reader.get();
+                reader.read();
+                while (reader.get() >= '0' && reader.get() <= '9'){
+                    name += reader.get();
+                    reader.read();
+                }
+                list.add(new Element(name, Type.VALUE, null));
+                System.out.println(name);
+                if(reader.get() == '&'){
+                    System.out.println("AND");
+                    list.add(new Element(null, Type.AND, null));
+                    reader.read();
+                    continue;
+                }
+                else if(reader.get() == '|'){
+                    list.add(new Element(null, Type.OR, null));
+                    System.out.println("OR");
+                    reader.read();
+                    continue;
+                }
+                else if(reader.get() == '='){
+                    reader.read();
+                    if(reader.get() == '>'){
+                        list.add(new Element(null, Type.IMP, null));
+                        System.out.println("IMP");
+                        reader.read();
+                        continue;
+                    }
+                    else {
+                        System.out.println("Ошибка при попытке считать операцию");
+                        throw new Exception();
+                    }
+                }
+                else if(reader.isInputState()){
+                    if(reader.get() == ')' && skCount == 0 || reader.get() != ')') {
+                        System.out.println("Ошибка перехода после считывания переменной");
+                        throw new Exception();
+                    }
+                }
+                else{
+                    System.out.println("END_INPUT");
+                    return;
+                }
             }
             else if(reader.get() == '!'){
-                not();
+                System.out.println("NOT");
+                list.add(new Element(null, Type.NOT, null));
+                reader.read();
+                continue;
             }
             else if(reader.get() == '('){
-                open();
+                skCount++;
+                System.out.println("OPEN");
+                list.add(new Element(null, Type.OPEN, null));
+                reader.read();
+                startState();
+                if(reader.get() == ')') {
+                    skCount--;
+                    list.add(new Element(null, Type.CLOSE, null));
+                    System.out.println("CLOSE");
+                    reader.read();
+                    if(reader.isInputState()){
+                        if(reader.get() == '&'){
+                            System.out.println("AND");
+                            list.add(new Element(null, Type.AND, null));
+                            reader.read();
+                            continue;
+                        }
+                        else if(reader.get() == '|'){
+                            list.add(new Element(null, Type.OR, null));
+                            System.out.println("OR");
+                            reader.read();
+                            continue;
+                        }
+                        else if(reader.get() == '='){
+                            reader.read();
+                            if(reader.get() == '>'){
+                                list.add(new Element(null, Type.IMP, null));
+                                System.out.println("IMP");
+                                reader.read();
+                                continue;
+                            }
+                            else {
+                                System.out.println("Ошибка при попытке считать операцию");
+                                throw new Exception();
+                            }
+                        }
+                        else if(reader.get() != ')' || skCount == 0) {
+                            System.out.println("Ошибка при попытке перейти к считыванию операции");
+                            throw new Exception();
+                        }
+                    }else{
+                        System.out.println("END_INPUT");
+                        return;
+                    }
+                }else{
+                    System.out.println("не найдена закрывающая скобка");
+                    throw new Exception();
+                }
             }
             else if(reader.get() == '1'){
-                one();
+                list.add(new Element(null, Type.CONST, true));
+                System.out.println(true);
+                reader.read();
+                if(reader.get() == '&'){
+                    System.out.println("AND");
+                    list.add(new Element(null, Type.AND, null));
+                    reader.read();
+                    continue;
+                }
+                else if(reader.get() == '|'){
+                    list.add(new Element(null, Type.OR, null));
+                    System.out.println("OR");
+                    reader.read();
+                    continue;
+                }
+                else if(reader.get() == '='){
+                    reader.read();
+                    if(reader.get() == '>'){
+                        list.add(new Element(null, Type.IMP, null));
+                        System.out.println("IMP");
+                        reader.read();
+                        continue;
+                    }
+                    else {
+                        System.out.println("Ошибка при попытке считать операцию");
+                        throw new Exception();
+                    }
+                }
+                else if(reader.isInputState()){
+                    if(reader.get() == ')' && skCount == 0 || reader.get() != ')') {
+                        System.out.println("Ошибка перехода после считывания переменной");
+                        throw new Exception();
+                    }
+                }
+                else{
+                    System.out.println("END_INPUT");
+                    return;
+                }
             }
             else if(reader.get() == '0'){
-                zero();
+                list.add(new Element(null, Type.CONST, false));
+                System.out.println(false);
+                reader.read();
+                if(reader.get() == '&'){
+                    System.out.println("AND");
+                    list.add(new Element(null, Type.AND, null));
+                    reader.read();
+                    continue;
+                }
+                else if(reader.get() == '|'){
+                    list.add(new Element(null, Type.OR, null));
+                    System.out.println("OR");
+                    reader.read();
+                    continue;
+                }
+                else if(reader.get() == '='){
+                    reader.read();
+                    if(reader.get() == '>'){
+                        list.add(new Element(null, Type.IMP, null));
+                        System.out.println("IMP");
+                        reader.read();
+                        continue;
+                    }
+                    else {
+                        System.out.println("Ошибка при попытке считать операцию");
+                        throw new Exception();
+                    }
+                }
+                else if(reader.isInputState()){
+                    if(reader.get() == ')' && skCount == 0 || reader.get() != ')') {
+                        System.out.println("Ошибка перехода после считывания переменной");
+                        throw new Exception();
+                    }
+                }
+                else{
+                    System.out.println("END_INPUT");
+                    return;
+                }
             }
             else{
                 System.out.println("ошибка в состоянии считывания скобки, отрицания, переменной или константы");
                 throw new Exception();
             }
-        }else{
-            System.out.println("ошибка в состоянии считывания скобки, отрицания, переменной или константы");
-            throw new Exception();
         }
+        System.out.println("ошибка в состоянии считывания скобки, отрицания, переменной или константы");
+        throw new Exception();
     }
 
-    private void not() throws Exception {
-        System.out.println("NOT");
-        list.add(new Element(null, Type.NOT, null));
-        reader.read();
-        startState();
-    }
-    private void open() throws Exception {
-        skCount++;
-        System.out.println("OPEN");
-        list.add(new Element(null, Type.OPEN, null));
-        reader.read();
-        startState();
-        close();
-    }
-    private void close() throws Exception {
-        if(reader.get() == ')') {
-            skCount--;
-            list.add(new Element(null, Type.CLOSE, null));
-            System.out.println("CLOSE");
-            reader.read();
-            if(reader.isInputState()){
-                if(reader.get() == '&'){
-                    and();
-                }
-                else if(reader.get() == '|'){
-                    or();
-                }
-                else if(reader.get() == '='){
-                    imp();
-                }
-                else if(reader.get() != ')' || skCount == 0) {
-                    System.out.println("Ошибка при попытке перейти к считыванию операции");
-                    throw new Exception();
-                }
-            }else{
-                System.out.println("END_INPUT");
-            }
-        }else{
-            System.out.println("не найдена закрывающая скобка");
-            throw new Exception();
-        }
-    }
-
-    private void letter() throws Exception {
-        String name = "" + reader.get();
-        reader.read();
-        while (reader.get() >= '0' && reader.get() <= '9'){
-            name += reader.get();
-            reader.read();
-        }
-        list.add(new Element(name, Type.VALUE, null));
-        System.out.println(name);
-        if(reader.get() == '&'){
-            and();
-        }
-        else if(reader.get() == '|'){
-            or();
-        }
-        else if(reader.get() == '='){
-            imp();
-        }
-        else if(reader.isInputState()){
-            if(reader.get() == ')' && skCount == 0 || reader.get() != ')') {
-                System.out.println("Ошибка перехода после считывания переменной");
-                throw new Exception();
-            }
-        }
-        else{
-            System.out.println("END_INPUT");
-        }
-    }
-
-    private void and() throws Exception {
-        System.out.println("AND");
-        list.add(new Element(null, Type.AND, null));
-        reader.read();
-        startState();
-    }
-
-    private void or() throws Exception {
-        list.add(new Element(null, Type.OR, null));
-        System.out.println("OR");
-        reader.read();
-        startState();
-    }
-    private void imp() throws Exception {
-        reader.read();
-        if(reader.get() == '>'){
-            list.add(new Element(null, Type.IMP, null));
-            System.out.println("IMP");
-            reader.read();
-            startState();
-        }
-        else {
-            System.out.println("Ошибка при попытке считать операцию");
-            throw new Exception();
-        }
-    }
-
-
-    private void one() throws Exception {
-        list.add(new Element(null, Type.CONST, true));
-        System.out.println(true);
-        reader.read();
-        if(reader.get() == '&'){
-            and();
-        }
-        else if(reader.get() == '|'){
-            or();
-        }
-        else if(reader.get() == '='){
-            imp();
-        }
-        else if(reader.isInputState()){
-            if(reader.get() == ')' && skCount == 0 || reader.get() != ')') {
-                System.out.println("Ошибка перехода после считывания переменной");
-                throw new Exception();
-            }
-        }
-        else{
-            System.out.println("END_INPUT");
-        }
-    }
-    private void zero() throws Exception {
-        list.add(new Element(null, Type.CONST, false));
-        System.out.println(false);
-        reader.read();
-        if(reader.get() == '&'){
-            and();
-        }
-        else if(reader.get() == '|'){
-            or();
-        }
-        else if(reader.get() == '='){
-            imp();
-        }
-        else if(reader.isInputState()){
-            if(reader.get() == ')' && skCount == 0 || reader.get() != ')') {
-                System.out.println("Ошибка перехода после считывания переменной");
-                throw new Exception();
-            }
-        }
-        else{
-            System.out.println("END_INPUT");
-        }
-    }
 
     public ArrayList<Element> getRequest(){
         return list;
--- src/LogicTree.java
+++ src/LogicTree.java
@@ -1,121 +1,16 @@
 import java.util.ArrayList;
 
-public class LogicTree {
-    private Operand root;
+class LogicTree {
+    private Node root;
 
-    private Operand add(ArrayList<Element> logStr){
-        Type operation;
-        int scobe=0;
-        int currLowPriority = 0;
-        if(logStr.size()==2){
-            if(logStr.get(0).type==Type.NOT){
-                return new Operand(null, add(new ArrayList<>(logStr.subList(1,2))), null, Type.NOT, null);
-            }
-            else {
-                System.out.println("magick");
-                return null;
-            }
-        }else if(logStr.size()==1){
-            return new Operand(logStr.get(0).value, null, null, logStr.get(0).type, logStr.get(0).name);
-        }
-        else{
-            for (int i = 0; i < logStr.size(); i++) {
-                operation = logStr.get(i).type;
-                if (logStr.get(i).type == Type.OPEN) {
-                    scobe++;
-                    continue;
-                }
-                if (logStr.get(i).type == Type.CLOSE) {
-                    scobe--;
-                    continue;
-                }
-                if (scobe == 0) {
-                    if (operation == Type.IMP) {
-                        currLowPriority = i;
-                    } else {
-                        if ((operation == Type.OR) && (logStr.get(currLowPriority).type != Type.IMP)) {
-                            currLowPriority = i;
-                        } else {
-                            if (currLowPriority == 0 || ((operation == Type.AND) && (logStr.get(currLowPriority).type == Type.AND))) {
-                                currLowPriority = i;
-                            }
-                        }
-                    }
-                }
-            }
-            ArrayList<Element> logLeft=killScobka(new ArrayList<>(logStr.subList(0,currLowPriority)));
-            ArrayList<Element> logRight=killScobka(new ArrayList<>(logStr.subList(currLowPriority+1,logStr.size())));
-            return new Operand(null,add(logLeft),add(logRight),logStr.get(currLowPriority).type,null);
-        }
-    }
-
-    private void isNot(ArrayList<Element> logStr){
-
-    }
-
-    private ArrayList<Element> killScobka(ArrayList<Element> arrayList){
-        if(arrayList.size() > 0 && arrayList.get(0).type == Type.OPEN){
-            int skLvl = 0;
-            for (Element o : arrayList){
-                if(o.type == Type.CLOSE)
-                    skLvl--;
-                if(o.type == Type.OPEN)
-                    skLvl++;
-                if(skLvl == 0 && arrayList.indexOf(o) != arrayList.size()-1) {
-                    return arrayList;
-                }
-            }
-            System.out.println("***********************************");
-            System.out.println(arrayList);
-            System.out.println("killSkobka");
-            arrayList.remove(arrayList.size()-1);
-            arrayList.remove(0);
-            System.out.println(arrayList);
-            System.out.println("***********************************");
-        }
-        return arrayList;
-    }
-
-    public LogicTree(ArrayList<Element> logStr){
-        killScobka(logStr);
-        root = add(logStr);
-    }
-
-    private class Operand{
-        Boolean value;
-        Operand leftOperand;
-        Operand rightOperand;
-        Type operation;
-        String name;
-
-        Operand(Boolean value,Operand leftOperand,Operand rightOperand,Type operation,String name){
-            this.value = value;
-            this.leftOperand = leftOperand;
-            this.rightOperand = rightOperand;
-            this.operation = operation;
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            String request = "";
-            if(value != null)
-                request += value;
-            else if (name != null)
-                request += name;
-            else{
-                request = " " +operation + " ";
-            }
-            return request;
-        }
-    }
-
-    public void out(){
+    void out(){
         out(root);
     }
 
-    private void out(Operand e) {
-        if (e.operation != Type.NOT) {
+    private void out(Node e) {
+        if(e == null)
+            return;
+        if (e.type != Type.NOT) {
             if (e.leftOperand != null || e.rightOperand != null)
                 System.out.print("(");
             if (e.leftOperand != null) {
@@ -132,6 +27,240 @@ public class LogicTree {
             out(e.leftOperand);
         }
     }
+    private static boolean isOperation(Type type){
+        return (type == Type.IMP) || (type == Type.OR) || (type == Type.AND) || (type == Type.NOT);
+    }
 
+    private static boolean lowPriority(Type currOperation, Type operation){ //возвращает истину,
+       if(!isOperation(currOperation)){                                      //если первый параметр менее приоритетен,
+           return false;                                                     //чем второй
+       }
+       if(operation==Type.NOT){
+           return true;
+       }
+       if(currOperation == Type.IMP){
+           return true;
+       }
+       if((currOperation==Type.OR)&&(operation!=Type.IMP)){
+           return true;
+       }
+       if((currOperation==Type.AND)&&(operation!=Type.OR)&&(operation!=Type.IMP)){
+           return true;
+       }
+       return false;
+    }
+
+    private Node add(ArrayList<Element> logStr){
+        killScobka(logStr);
+        Type type = logStr.get(0).type;
+        if(logStr.size()<=2){
+            int vInd = logStr.size()-1;
+            Node op = new Node(logStr.get(vInd).value,null,null,logStr.get(vInd).type,logStr.get(vInd).name);
+            if(type==Type.NOT){
+//                if(killNotBeforeConst(logStr, 0))
+//                    return add(logStr);
+                return new Node(null,op,null,Type.NOT,null);
+            }
+            else{
+                return op;
+            }
+        }
+        else{
+            int scobe=0;
+            int currLowPriority = -1;
+            Type currLowType = Type.NOT;
+            for (int i = 0; i < logStr.size(); i++) {
+                type = logStr.get(i).type;
+                if(currLowPriority!=-1){
+                    currLowType = logStr.get(currLowPriority).type;
+                }
+                if (logStr.get(i).type == Type.OPEN) {
+                    scobe++;
+                    continue;
+                }
+                if (logStr.get(i).type == Type.CLOSE) {
+                    scobe--;
+                    continue;
+                }
+                if (scobe == 0) {
+                    if (isOperation(type)) {
+                        if(lowPriority(type,currLowType)){
+                            currLowPriority=i;
+                        }
+                    }
+                }
+            }
+//            if(currLowPriority != -1) {
+//                if (killMoreThenOneNot(logStr, currLowPriority))
+//                    return add(logStr);
+//                if (killNotBeforeConst(logStr, currLowPriority))
+//                    return add(logStr);
+//                if (killOrWithConst(logStr, currLowPriority))
+//                    return add(logStr);
+//            }
+
+            ArrayList<Element> logRight = new ArrayList<>(logStr.subList(currLowPriority+1,logStr.size()));
+            if(currLowType==Type.NOT){
+                return new Node(null,add(logRight),null,currLowType,null);
+            }
+            else{
+                ArrayList<Element> logLeft = new ArrayList<>(logStr.subList(0,currLowPriority));
+                return new Node(null,add(logLeft),add(logRight),currLowType,null);
+            }
+        }
+    }
+
+//    static ArrayList<Element> getSubLeft(ArrayList<Element> logStr, int rightNotIncl){
+//        ArrayList<Element> req = new ArrayList<>();
+//        if(logStr.get(rightNotIncl-1).type == Type.CONST || logStr.get(rightNotIncl-1).type == Type.VALUE){
+//            req.add(logStr.get(rightNotIncl-1));
+//        }
+//        if(logStr.get(rightNotIncl-1).type == Type.CLOSE){
+//            req.add(logStr.get(rightNotIncl-1));
+//            int skLvl = 1;
+//            int id = rightNotIncl-2;
+//            while (skLvl != 0){
+//                if (logStr.get(id).type == Type.CLOSE)
+//                    skLvl++;
+//                if(logStr.get(id).type == Type.OPEN)
+//                    skLvl--;
+//                req.add(logStr.get(id));
+//                id--;
+//            }
+//        }
+//        return req;
+//    }
+//
+//    static ArrayList<Element> getSubRight(ArrayList<Element> logStr, int leftNotIncl){
+//        ArrayList<Element> req = new ArrayList<>();
+//        if(logStr.get(leftNotIncl+1).type == Type.CONST || logStr.get(leftNotIncl+1).type == Type.VALUE){
+//            req.add(logStr.get(leftNotIncl+1));
+//        }
+//        if(logStr.get(leftNotIncl+1).type == Type.OPEN){
+//            req.add(logStr.get(leftNotIncl+1));
+//            int skLvl = 1;
+//            int id = leftNotIncl+2;
+//            while (skLvl != 0){
+//                if (logStr.get(id).type == Type.OPEN)
+//                    skLvl++;
+//                if(logStr.get(id).type == Type.CLOSE)
+//                    skLvl--;
+//                req.add(logStr.get(id));
+//                id++;
+//            }
+//        }
+//        return req;
+//    }
+//
+//    static boolean killOrWithConst(ArrayList<Element> logStr, int or){
+//        System.out.println("KILLLLL OR CONST");
+//        if(logStr.get(or).type == Type.OR) {
+//            if(logStr.get(or+1).type == Type.CONST){
+//                if(logStr.get(or+1).value){
+//                    logStr.remove(or);
+//                    for (Element e : getSubLeft(logStr, or))
+//                        logStr.remove(e);
+//                }else {
+//                    logStr.remove(or+1);
+//                    logStr.remove(or);
+//                }
+//                return true;
+//            }
+//            else if(logStr.get(or-1).type == Type.CONST){
+//                if(logStr.get(or-1).value){
+//                    for (Element e : getSubRight(logStr, or))
+//                        logStr.remove(e);
+//                    logStr.remove(or);
+//                }else {
+//                    logStr.remove(or);
+//                    logStr.remove(or-1);
+//                    System.out.println(logStr);
+//                }
+//                return true;
+//            }
+//            return false;
+//        }else{
+//            return false;
+//        }
+//    }
+//
+//    static boolean killNotBeforeConst(ArrayList<Element> logStr, int not){
+//        System.out.println("KILLLLL NOT CONST");
+//        if(logStr.get(not).type == Type.NOT && logStr.get(not+1).type == Type.CONST) {
+//            if(logStr.get(not+1).value){
+//                logStr.get(not+1).value = false;
+//                logStr.remove(not);
+//            }else{
+//                logStr.get(not+1).value = true;
+//                logStr.remove(not);
+//            }
+//            return true;
+//        }else{
+//            return false;
+//        }
+//    }
+//
+//    static boolean killMoreThenOneNot(ArrayList<Element> logStr, int rightNot){
+//        System.out.println("KILLLLL NOT");
+//        if(rightNot > 0 && logStr.get(rightNot).type == Type.NOT && logStr.get(rightNot-1).type == Type.NOT) {
+//            while (rightNot > 0 && logStr.get(rightNot).type == Type.NOT && logStr.get(rightNot - 1).type == Type.NOT) {
+//                logStr.remove(rightNot);
+//                logStr.remove(rightNot - 1);
+//                rightNot -= 2;
+//            }
+//            return true;
+//        }else{
+//            return false;
+//        }
+//    }
+
+     static void killScobka(ArrayList<Element> arrayList){
+        if(arrayList.size() > 0 && arrayList.get(0).type == Type.OPEN){
+            int skLvl = 0;
+            for (Element o : arrayList){
+                if(o.type == Type.CLOSE)
+                    skLvl--;
+                if(o.type == Type.OPEN)
+                    skLvl++;
+                if(skLvl == 0 && arrayList.indexOf(o) != arrayList.size()-1) {
+                    return;
+                }
+            }
+            arrayList.remove(arrayList.size()-1);
+            arrayList.remove(0);
+        }
+    }
+
+    LogicTree(ArrayList<Element> logStr){
+        root = add(logStr);
+    }
+
+    private class Node{
+        Boolean value;
+        Node leftOperand;
+        Node rightOperand;
+        Type type;
+        String name;
+
+        Node(Boolean value,Node leftNode,Node rightNode,Type type,String name){
+            this.value = value;
+            this.leftOperand = leftNode;
+            this.rightOperand = rightNode;
+            this.type = type;
+            this.name = name;
+        }
+        @Override
+        public String toString() {
+            String request = "";
+            if(value != null)
+                request += value;
+            else if (name != null)
+                request += name;
+            else{
+                request = " " + type + " ";
+            }
+            return request;
+        }
+    }
 }
 
